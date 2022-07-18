994c66fd4f7980b82f3d5e75a676e09a23182449
junwvwv <wvwv0612@gmail.com>
2022-07-18 21:45:48 +0900

    jpa example

--- jpa/jpa/src/main/java/com/example/jpa/repository/ProductRepository.java
+++ jpa/jpa/src/main/java/com/example/jpa/repository/ProductRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -99,5 +101,12 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Queryds
 
     @Query("select p.name, p.price, p.stock from Product p where p.name = :name")
     List<Object[]> findByNameParam2(@Param("name") String name);
+
+    @Query("update Product p set p.name = :name where p.id = :id")
+    @Modifying
+    void updateNameById(@Param("name") String name, @Param("id") Long id);
+
+    @Query(value = "select * from product p where p.name =:name", nativeQuery = true)
+    List<Product> findByName2(String name);
 
 }
